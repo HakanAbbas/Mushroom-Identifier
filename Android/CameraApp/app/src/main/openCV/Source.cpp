2a34967b6cbd3903813c34ccdcaa5c540c601ca1
Hakan <Hakanabbas@live.de>
2017-03-04 17:29:50 +0100

    Implementierung Android + Verwdendete Technologien update

--- Android/CameraApp/app/src/main/openCV/Source.cpp
+++ Android/CameraApp/app/src/main/openCV/Source.cpp
@@ -6,7 +6,6 @@ using namespace cv;
 using namespace std;
 
 
-
 int main(int argc, char** argv)
 {
 	vector<Pilz> mushlist=detectMushroom("..\\..\\..\\..\\..\\..\\common\\data\\schwammerl.xml", "..\\..\\..\\..\\..\\..\\common\\data\\mushroom_cascade.xml", imread("..\\..\\..\\..\\..\\..\\common\\data\\eiersch.jpg"));
