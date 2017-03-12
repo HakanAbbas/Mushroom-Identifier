4302e0e7de8cb48be8195e163e8b7d66227f2f04
MarkusArb <markus.arbeithuber@hotmail.com>
2017-03-12 21:29:29 +0100

    iOS commit

---
+++ iOS/iOSApp/OpenCVWrapper.mm
@@ -0,0 +1,184 @@
+//
+//  OpenCVWrapper.m
+//  CameraApp
+//
+//  Created by mbkair02 on 31.10.16.
+//  Copyright © 2016 user. All rights reserved.
+//
+
+#import "OpenCVWrapper.h"
+#import <opencv2/opencv.hpp>
+#import <UIKit/UIKit.h>
+#import "PilzC.h"
+
+//#import "OpenCV.h"
+
+#include <opencv2/core/core.hpp>
+//#include <iostream>
+#include "opencv2/opencv.hpp"
+
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+#include <cstdlib>
+#include <iostream>
+#include <stdio.h>
+//#include <windows.h>
+#include <codecvt>
+//#include <string>
+#include <list>
+
+#include "Source.cpp"
+
+using namespace cv;
+using namespace std;
+
+//OpenCVWrapper.h File Implementierung
+@implementation OpenCVWrapper
+
++(NSMutableArray<PilzC *> *) DetectByColor:(UIImage*) img : (NSString*) xmlpath1 : (NSString*) xmlpath2
+{
+    std::string xmlpathh1 = std::string([xmlpath1 UTF8String]);
+    std::string xmlpathh2 = std::string([xmlpath2 UTF8String]);
+    
+    vector<Pilz> mushlist = detectMushroom(xmlpathh1, xmlpathh2, convertToMat(img));
+    
+    //ret = [NSString stringWithFormat:@"%lu", mushlist2.size()];
+    
+    NSMutableArray<PilzC *> *arr = [[NSMutableArray alloc] init];
+    //PilzC *c = [[PilzC alloc]init];;
+    //PilzC box1 = [[PilzC alloc]init];
+    PilzC *c = [[PilzC alloc] init];
+    
+    for(int i = 0; i < mushlist.size(); i++){
+        c.name = [NSString stringWithUTF8String:mushlist[i].name.c_str()];
+        c.wiki = [NSString stringWithUTF8String:mushlist[i].wiki.c_str()];
+        c.stiel = [NSString stringWithUTF8String:mushlist[i].stalk.c_str()];
+        c.lamellen = mushlist[i].lamell;
+        c.knolle = mushlist[i].nodule;
+        c.giftigkeitt = mushlist[i].poisonous;
+        c.rund = mushlist[i].roud;
+        
+        cout << c.name;
+        
+        [arr addObject: c];
+        
+        c = [[PilzC alloc] init];
+    }
+    
+    
+    //return convertToUIImage(image);
+    return arr;
+    
+}
+
++(NSMutableArray<PilzC *> *) allMushs: (NSString*) xmlpath{
+    std::string xmlpathh = std::string([xmlpath UTF8String]);
+    
+    vector<Pilz> mushlist = readxml(string(xmlpathh)); //Liste aller gelesenen Pilze
+    
+    NSMutableArray<PilzC *> *arr = [[NSMutableArray alloc] init];
+    //PilzC *c = [[PilzC alloc]init];;
+    //PilzC box1 = [[PilzC alloc]init];
+    PilzC *c = [[PilzC alloc] init];
+    
+    for(int i = 0; i < mushlist.size(); i++){
+        c.name = [NSString stringWithUTF8String:mushlist[i].name.c_str()];
+        c.wiki = [NSString stringWithUTF8String:mushlist[i].wiki.c_str()];
+        c.stiel = [NSString stringWithUTF8String:mushlist[i].stalk.c_str()];
+        c.lamellen = mushlist[i].lamell;
+        c.knolle = mushlist[i].nodule;
+        c.giftigkeitt = mushlist[i].poisonous;
+        c.rund = mushlist[i].roud;
+        
+        cout << c.name;
+        
+        [arr addObject: c];
+        
+        c = [[PilzC alloc] init];
+    }
+    
+    return arr;
+}
+
+
+Mat convertToMat(UIImage* img)
+{
+    CGColorSpaceRef colorSpace = CGImageGetColorSpace(img.CGImage);
+    CGFloat cols = img.size.width;
+    CGFloat rows = img.size.height;
+    
+    cv::Mat cvMat(rows, cols, CV_8UC4); // 8 bits per component, 4 channels (color channels + alpha)
+    
+    CGContextRef contextRef = CGBitmapContextCreate(cvMat.data,                 // Pointer to  data
+                                                    cols,                       // Width of bitmap
+                                                    rows,                       // Height of bitmap
+                                                    8,                          // Bits per component
+                                                    cvMat.step[0],              // Bytes per row
+                                                    colorSpace,                 // Colorspace
+                                                    kCGImageAlphaNoneSkipLast |
+                                                    kCGBitmapByteOrderDefault); // Bitmap info flags
+    
+    CGContextDrawImage(contextRef, CGRectMake(0, 0, cols, rows), img.CGImage);
+    CGContextRelease(contextRef);
+    
+    return cvMat;
+}
+
+UIImage* convertToUIImage(Mat image)
+{
+    NSData *data = [NSData dataWithBytes:image.data length:image.elemSize()*image.total()];
+    
+    CGColorSpaceRef colorSpace;
+    
+    if (image.elemSize() == 1) {
+        colorSpace = CGColorSpaceCreateDeviceRGB();
+    } else {
+        colorSpace = CGColorSpaceCreateDeviceRGB();
+    }
+    
+    CGDataProviderRef provider = CGDataProviderCreateWithCFData((__bridge CFDataRef)data);
+    
+    // Creating CGImage from cv::Mat
+    CGImageRef imageRef = CGImageCreate(image.cols,                                 //width
+                                        image.rows,                                 //height
+                                        8,                                          //bits per component
+                                        8 * image.elemSize(),                       //bits per pixel
+                                        image.step[0],                            //bytesPerRow
+                                        colorSpace,                                 //colorspace
+                                        kCGImageAlphaNone|kCGBitmapByteOrderDefault,// bitmap info
+                                        provider,                                   //CGDataProviderRef
+                                        NULL,                                       //decode
+                                        false,                                      //should interpolate
+                                        kCGRenderingIntentDefault                   //intent
+                                        );
+    
+    
+    // Getting UIImage from CGImage
+    UIImage *finalImage = [UIImage imageWithCGImage:imageRef];
+    CGImageRelease(imageRef);
+    CGDataProviderRelease(provider);
+    CGColorSpaceRelease(colorSpace);
+    
+    return finalImage;
+}
+
+
+@end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
