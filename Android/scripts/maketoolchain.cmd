aa6c5c3fe8990fc9fc20a949b37918ac0362d03d
qrzbrz <qrzbrz@yahoo.de>
2017-03-13 15:00:08 +0100

    ndk destination changed

--- Android/scripts/maketoolchain.cmd
+++ Android/scripts/maketoolchain.cmd
@@ -1,1 +1,1 @@
-python %NDK%\build\tools\make_standalone_toolchain.py --arch x86_64 --api 24 --stl=stlport --unified-headers --install-dir C:\Users\aberger\Desktop\mist
+python %NDK%\build\tools\make_standalone_toolchain.py --arch x86_64 --api 24 --stl=stlport --unified-headers --install-dir %HOMEPATH%\Desktop\ndk
