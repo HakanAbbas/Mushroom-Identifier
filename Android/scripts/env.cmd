48a93a53214380b48b870bacad5a0a006bdc8eb1
qrzbrz <qrzbrz@yahoo.de>
2017-03-13 14:51:53 +0100

    android toolchain

---
+++ Android/scripts/env.cmd
@@ -0,0 +1,13 @@
+SET JAVA_HOME=C:\Program Files\Java\jdk1.8.0_121
+SET M2_HOME=C:\Program Files\maven
+SET MAVEN_HOME=%M2_HOME%
+SET NODE_HOME=%APPDATA%\npm
+SET RUBY_HOME=C:\ruby\Ruby23-x64
+SET RUBY_DEV=C:\ruby\rubydevkit
+SET ANDROID_NDK=%HOMEDRIVE%%HOMEPATH%\Documents\swdev\ndk\android-ndk-r14
+SET NDK=%ANDROID_NDK%
+SET PYTHON_HOME=C:\Users\aberger\AppData\Local\Programs\Python\Python36
+SET CMAKE_HOME=C:\Program Files\CMake
+
+SET PATH=%JAVA_HOME%\bin;%ProgramFiles%\Git\mingw64\bin;%NODE_HOME%;%ProgramFiles%\nodejs;%M2_HOME%\bin;%NDK%\bin;%PYTHON_HOME%;%RUBY_HOME%\bin;%RUBY_DEV%\bin;%CMAKE_HOME%\bin;%SystemRoot%\System32;%SystemRoot%
+
