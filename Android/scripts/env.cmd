d77fdf537b412d27bd7bab240fd713b56a1dfd2f
Hakan <Hakanabbas@live.de>
2017-03-13 15:48:17 +0100

    env-variable changed

--- Android/scripts/env.cmd
+++ Android/scripts/env.cmd
@@ -4,9 +4,9 @@ SET MAVEN_HOME=%M2_HOME%
 SET NODE_HOME=%APPDATA%\npm
 SET RUBY_HOME=C:\ruby\Ruby23-x64
 SET RUBY_DEV=C:\ruby\rubydevkit
-SET ANDROID_NDK=%HOMEDRIVE%%HOMEPATH%\Documents\swdev\ndk\android-ndk-r14
+SET ANDROID_NDK=%HOMEDRIVE%%HOMEPATH%\Documents\Android\ndk\android-ndk-r14
 SET NDK=%ANDROID_NDK%
-SET PYTHON_HOME=C:\Users\aberger\AppData\Local\Programs\Python\Python36
+SET PYTHON_HOME=C:\Users\Hakan\AppData\Local\Programs\Python\Python36-32
 SET CMAKE_HOME=C:\Program Files\CMake
 
 SET PATH=%JAVA_HOME%\bin;%ProgramFiles%\Git\mingw64\bin;%NODE_HOME%;%ProgramFiles%\nodejs;%M2_HOME%\bin;%NDK%\bin;%PYTHON_HOME%;%RUBY_HOME%\bin;%RUBY_DEV%\bin;%CMAKE_HOME%\bin;%SystemRoot%\System32;%SystemRoot%
