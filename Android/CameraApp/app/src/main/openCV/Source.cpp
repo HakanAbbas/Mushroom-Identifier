58260e093d505244e25833213f3655dac54abce4
Hakan <Hakanabbas@live.de>
2017-03-15 17:01:58 +0100

    Android update

--- Android/CameraApp/app/src/main/openCV/Source.cpp
+++ Android/CameraApp/app/src/main/openCV/Source.cpp
@@ -10,7 +10,6 @@ int main(int argc, char** argv)
 {
 	vector<Pilz> mushlist=detectMushroom("..\\..\\..\\..\\..\\..\\common\\data\\schwammerl.xml", "..\\..\\..\\..\\..\\..\\common\\data\\mushroom_cascade.xml", imread("..\\..\\..\\..\\..\\..\\common\\data\\Eierschwammerl.jpgerl.jpg"));
 	cout<<mushlist.size();
-
 	waitKey(0);
 	return 0;
 }
