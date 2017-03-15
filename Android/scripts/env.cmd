ea29c51ea6e55a26d9cf7542edde29e622a8e4f1
Hakan <Hakanabbas@live.de>
2017-03-15 15:51:12 +0100

    env variable changes

--- Android/scripts/env.cmd
+++ Android/scripts/env.cmd
@@ -2,8 +2,13 @@
 SET ANDROID_SDK=%HOMEDRIVE%\Android\sdk\
 SET ANDROID_NDK=%HOMEDRIVE%%HOMEPATH%\Documents\swdev\ndk\android-ndk-r14
 SET NDK=%ANDROID_NDK%
+<<<<<<< HEAD
 SET PYTHON_HOME=C:\Users\aberger\AppData\Local\Programs\Python\Python36
 SET STANDALONE_TOOLCHAIN=%HOMEPATH%\Desktop\ndk
+=======
+SET PYTHON_HOME=C:\Users\Hakan\AppData\Local\Programs\Python\Python36-32
+SET STANDALONE_TOOLCHAIN=C:\Users\Hakan\Desktop\ndk
+>>>>>>> env variable changes
 SET CMAKE_HOME=C:\Program Files\CMake
 
 SET CC=clang
